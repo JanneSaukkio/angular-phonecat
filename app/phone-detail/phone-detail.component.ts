fed2a66bccd849412f56d96d0816939435dbec05
Janne Saukkio <janne.saukkio@remion.com>
2025-11-14 20:24:42 +0200

    Step 3 - Convert components to classes

--- app/phone-detail/phone-detail.component.ts
+++ app/phone-detail/phone-detail.component.ts
@@ -1,21 +1,36 @@
 'use strict';
 
+class PhoneDetailComponent implements angular.IComponentOptions {
+  controller: angular.IControllerConstructor;
+  templateUrl = 'phone-detail/phone-detail.template.html';
+
+  constructor() {
+      this.controller = PhoneDetailController;
+  }
+}
+
+class PhoneDetailController implements angular.IComponentController {
+  phone: any;
+  mainImageUrl: string;
+
+  static $inject = ['$routeParams', 'Phone'];
+  constructor(private $routeParams: angular.route.IRouteParamsService, private Phone: any) {
+    "ngInject";
+  }
+
+  $onInit(): void {
+    this.Phone.get({phoneId: this.$routeParams.phoneId}, (phone) => {
+      this.phone = phone;
+      this.setImage(phone.images[0]);
+    });
+  }
+
+  setImage(imageUrl: string) {
+    this.mainImageUrl = imageUrl;
+  }
+}
+
 // Register `phoneDetail` component, along with its associated controller and template
 angular.
   module('phoneDetail').
-  component('phoneDetail', {
-    templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$routeParams', 'Phone',
-      function PhoneDetailController($routeParams: angular.route.IRouteParamsService, Phone) {
-        var self = this;
-        Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-          self.phone = phone;
-          self.setImage(phone.images[0]);
-        });
-
-        self.setImage = function setImage(imageUrl) {
-          self.mainImageUrl = imageUrl;
-        };
-      }
-    ]
-  });
+  component('phoneDetail', new PhoneDetailComponent());
--- app/phone-list/phone-list.component.ts
+++ app/phone-list/phone-list.component.ts
@@ -1,16 +1,32 @@
 'use strict';
 
+class PhoneListComponent implements angular.IComponentOptions {
+  controller: angular.IControllerConstructor;
+  templateUrl = 'phone-list/phone-list.template.html';
+
+  constructor() {
+      this.controller = PhoneListController;
+  }
+}
+
+class PhoneListController implements angular.IComponentController {
+  phones: any[] = [];
+  orderProp: string;
+
+  static $inject = ['Phone'];
+  constructor(private Phone: any) {
+    "ngInject";
+  }
+
+  $onInit(): void {
+    this.Phone.query({}, (phones) => {
+      this.phones = phones;
+    });
+    this.orderProp = 'age';
+  }
+}
+
 // Register `phoneList` component, along with its associated controller and template
 angular.
   module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
-        Phone.query({}, (phones) => {
-          this.phones = phones;
-        });
-        this.orderProp = 'age';
-      }
-    ]
-  });
+  component('phoneList', new PhoneListComponent());
